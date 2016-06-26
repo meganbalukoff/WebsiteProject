a0cfbb17897a8f096890d021ef41948ba3bf4c67
Megan <meganbalukoff@yahoo.com>
2016-06-26 04:11:32 +0000

    Additional changes

--- lib/assets/ReadFiles.php
+++ lib/assets/ReadFiles.php
@@ -1,8 +1,12 @@
 <?php
-$myfile = fopen("CreatedFile.txt", "w") or die("Unable to open file!");
-$txt = "Please enjoy the website!\n";
+$myfile = fopen("CreatedFile.txt", "a+") or die("Unable to open file!");
+$txt = "Hi \n";
+fwrite($myfile, $txt);
+$txt = "John \n";
 fwrite($myfile, $txt);
 $txt = "Hello \n";
+fwrite($myfile, $txt);
+$txt = "Please Enjoy the Website! \n";
 fwrite($myfile, $txt);
 fclose($myfile);
 ?>
